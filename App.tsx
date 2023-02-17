eadbe2eea5c00a463d8c2d6c5040f8d045dd0966
Daniyal Naeem <daniyalnaeem5@gmail.com>
2023-02-17 10:47:45 +0500

    First commit

---
+++ App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+function App(): JSX.Element {
+
+  return (
+ <View>
+  <Text>Main Screen</Text>
+ </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
+
+export default App;
