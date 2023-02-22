26e28793d8d979282cd23720fe414f5217e682c9
Daniyal Naeem <daniyalnaeem5@gmail.com>
2023-02-22 16:09:52 +0500

    common background

---
+++ src/common/Background.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import { ImageBackground, StyleSheet, KeyboardAvoidingView, StatusBar } from 'react-native'
+import { theme } from '../code/Theme'
+
+export default function Background({ children }) {
+  return (
+    <ImageBackground
+      source={require('../assets/svgs/PetMark')}
+      resizeMode="repeat"
+      style={styles.background}
+    >
+      <KeyboardAvoidingView style={styles.container} behavior="padding">
+        <StatusBar 
+        backgroundColor={"#fff"} 
+        barStyle={"dark-content"} 
+        translucent={true}/>
+        {children}
+      </KeyboardAvoidingView>
+    </ImageBackground>
+  )
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: theme.colors.surface,
+  },
+  container: {
+    flex: 1,
+    padding: 10,
+    width: '100%',
+    maxWidth: 340,
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop:150
+  },
+})
